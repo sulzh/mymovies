dd93d47433fbb1c5d478115cd3f45b94ee60dd3d
Andrew <privettandrey@gmail.com>
2018-10-13 14:47:24 +0300

    Updates

---
+++ precache-manifest.a0eb1dd351a654186ac21d1e627ff5ed.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc5d30de2b067999ca984ef5429cadea",
+    "url": "/mymovies/static/media/icon-search.cc5d30de.svg"
+  },
+  {
+    "revision": "08947b8ca8b866cebe906e3cdace9808",
+    "url": "/mymovies/static/media/icon-search-blue.08947b8c.svg"
+  },
+  {
+    "revision": "95a6c43249bbc4a126e624262cd8b91b",
+    "url": "/mymovies/static/media/icon-favorites-red.95a6c432.svg"
+  },
+  {
+    "revision": "34b63c8e7d6d0fc558fe65be671175c1",
+    "url": "/mymovies/static/media/icon-favorites-blue.34b63c8e.svg"
+  },
+  {
+    "revision": "c5d3956657095a72e4afc06fbd93720a",
+    "url": "/mymovies/static/media/icon-favorites-big.c5d39566.svg"
+  },
+  {
+    "revision": "6eb5f86761ee62331548",
+    "url": "/mymovies/static/js/runtime~main.6eb5f867.js"
+  },
+  {
+    "revision": "f7f119f46e5da94f4707",
+    "url": "/mymovies/static/js/main.f7f119f4.chunk.js"
+  },
+  {
+    "revision": "b5051975a78cf2c88a75",
+    "url": "/mymovies/static/js/1.b5051975.chunk.js"
+  },
+  {
+    "revision": "f7f119f46e5da94f4707",
+    "url": "/mymovies/static/css/main.3f5060dc.chunk.css"
+  },
+  {
+    "revision": "7990d42001f18fa995060df6772710d0",
+    "url": "/mymovies/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b08e23b459882fdfd41144244374cb1c.js"
+  "/mymovies/precache-manifest.a0eb1dd351a654186ac21d1e627ff5ed.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/mymovies/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
